32fc30f66158de21ef04774dca13d57c33dea608
Robert Spychala <robspychala@gmail.com>
2012-04-16 15:26:08 -0400

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
         
 setup(
     name='simpledict',
-    version='0.2.3',
+    version='0.2.4',
     description='Simple dictionary wrapper',
     author='Robert Spychala',
     author_email="robspychala@gmail.com",
